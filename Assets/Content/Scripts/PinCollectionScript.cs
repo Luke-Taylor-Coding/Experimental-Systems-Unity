bfb4b46909ff9b2f51dd1aa098c1d08bde199e01
Luke.Taylor <luketaylor1702@gmail.com>
2025-04-27 23:53:02 +0100

    Created score manager and UI
    The score manager is now functional, when a pin is dropped it will add 100 score to the manager, the manager then sees it has been updated and updates all UI in the list to show the new score.

--- Assets/Content/Scripts/PinCollectionScript.cs
+++ Assets/Content/Scripts/PinCollectionScript.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class PinCollectionScript : MonoBehaviour
 {
+    public ScoreManager m_scoreManager;
+    public float m_ScorePerPin = 100;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (m_scoreManager == null)
+        {
+            Debug.LogError("Score Manager is not set in Pin Destrotor Object!");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pin"))
         {
             other.gameObject.SetActive(false);
+            m_scoreManager.AddScore(m_ScorePerPin);
         }
         else if (other.gameObject.CompareTag("Ball"))
         {
--- Assets/Content/Scripts/ScoreManager.cs
+++ Assets/Content/Scripts/ScoreManager.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEditor;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private float m_score = 0;
+    public TextMeshProUGUI[] m_scoreUIs = null;
 
-    // Update is called once per frame
-    void Update()
+    public void ScoreUpdated()
     {
-        
+        foreach (var UI in m_scoreUIs)
+        {
+            UI.text = m_score.ToString();
+        }
+    }
+    public void AddScore(float Score)
+    {
+        m_score += Score;
+        ScoreUpdated();
+    }
+    public void MinusScore(float Score)
+    {
+        m_score -= Score;
+        ScoreUpdated();
+    }
+    public void SetScore(float Score)
+    {
+        m_score = Score;
+        ScoreUpdated();
+    }
+    public float GetScore()
+    {
+        return m_score;
     }
 }
