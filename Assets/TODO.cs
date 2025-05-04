efc67bfd2887fe30865f9d09333b198d88ef9f90
Luke.Taylor <luketaylor1702@gmail.com>
2025-05-04 01:24:35 +0100

    imported in a couple test textures

--- Assets/TODO.cs
+++ Assets/TODO.cs
@@ -2,7 +2,6 @@
 
 // BALL GAME
 // make pin layout better
-// prevent game from being restarted if active 
 // add hard mode scoring func to game manager
 // game loop (after time, go again if score reached)
 // add proper lighting 
