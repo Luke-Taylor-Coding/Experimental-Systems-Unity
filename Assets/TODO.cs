debd10df144d70e73b6a8364b7cb81e64d2f83a7
Luke.Taylor <luketaylor1702@gmail.com>
2025-05-08 14:24:25 +0100

    Stall window collider and back button working
    The stall window collider now allows balls to be passed through but not the player. The back button to the main menu is also now functional.

--- Assets/TODO.cs
+++ Assets/TODO.cs
@@ -2,20 +2,20 @@
 
 // Current Issues
 // lighting behind player issues
-// player can enter the stand
+// stand window issues
 // game button cant be pressed down
+// anchor issues 
+// 
 
 // BALL GAME
 // make pin layout better
 // Add anchor points           NEED TESTING
 // game loop (after time, go again if score reached)
 // add proper lighting 
-// game start and ended text    NEED TESTING
 // tweak ball physics 
 // Affordance!!!
 // add textures
 // add tutorials
-// add atmosphere 
 // add sounds and visual effects 
 
 
